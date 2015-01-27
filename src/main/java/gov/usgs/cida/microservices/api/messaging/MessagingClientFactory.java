aebf8a5e150407a0a064bc00f02122bac3bcccb6
thongsav-usgs <thongsav@usgs.gov>
2015-01-27 09:11:27 -0600

    Introduced some messaging stubbing, but holding off until we understand messaging better and know which messaging principles we want to maintain in our interfaces.

---
+++ src/main/java/gov/usgs/cida/microservices/api/messaging/MessagingClientFactory.java
@@ -0,0 +1,5 @@
+package gov.usgs.cida.microservices.api.messaging;
+
+public interface MessagingClientFactory {
+
+}
---
+++ src/main/java/gov/usgs/cida/microservices/messaging/rabbitmq/RabbitMqClient.java
@@ -0,0 +1,5 @@
+package gov.usgs.cida.microservices.messaging.rabbitmq;
+
+public class RabbitMqClient {
+
+}
